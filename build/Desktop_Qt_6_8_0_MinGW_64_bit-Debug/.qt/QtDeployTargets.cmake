30dd4973b47aec8ead1c16b30c889da02478c84f
luterofromtx <logan.prosperie@gmail.com>
2024-11-07 19:38:32 -0600

    Delete build directory
    The build directory and anything inside should not be here, else it will break the program for everyone

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ActivityPlanner_FILE C:/Users/edidi/OneDrive/Documents/TaskPlanner/ActivityPlanner/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ActivityPlanner.exe)
-set(__QT_DEPLOY_TARGET_ActivityPlanner_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ActivityPlanner_RUNTIME_DLLS C:/Qt2/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt2/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt2/6.8.0/mingw_64/bin/Qt6Core.dll)
