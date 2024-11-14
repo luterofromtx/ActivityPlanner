2fc4c13d351c881dfb52aad665165997f738f052
Dee <edidionganwan@my.unt.edu>
2024-11-14 14:42:33 -0600

    Update largecalendar.h for calendar task

--- largecalendar.h
+++ largecalendar.h
@@ -2,7 +2,9 @@
 #define LARGECALENDAR_H
 
 #include <QDialog>
-#include "datesettings.h"
+#include <QDate>
+#include <QVector>
+#include "task.h"
 
 namespace Ui {
 class LargeCalendar;
@@ -13,16 +15,20 @@ class LargeCalendar : public QDialog
     Q_OBJECT
 
 public:
-    explicit LargeCalendar(QWidget *parent = nullptr);
+    explicit LargeCalendar(QVector<Task> tasks, QWidget *parent = nullptr);  // Constructor that takes tasks and parent
     ~LargeCalendar();
+
+signals:
+    void dateSelected(const QDate &date);  // Signal to emit when a date is selected
 
 private slots:
     void on_CloseBtn_clicked();
-
     void on_calendarWidget_activated(const QDate &date);
 
 private:
     Ui::LargeCalendar *ui;
+    QVector<Task> taskList;  // List of tasks
 };
 
 #endif // LARGECALENDAR_H
+
