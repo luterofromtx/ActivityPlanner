b59c302dd9ea95ecf214eb6a8298db9371508f2d
Ysalapu24 <139722293+Ysalapu24@users.noreply.github.com>
2024-12-06 15:43:40 -0600

    Update timer.h
    changed the button funtions

--- timer.h
+++ timer.h
@@ -17,7 +17,7 @@ public:
     ~Timer();
 
 signals:
-    void dataAvailable(const QString &data); // Signal to send data back to MainTaskScreen
+    void dataAvailable(const QString &data); // Signal to notify parent widget
 
 private slots:
     void on_oneminute_clicked();
@@ -26,9 +26,16 @@ private slots:
     void on_fourtyfiveminutes_clicked();
     void on_onehour_clicked();
     void on_twohours_clicked();
+    void updateLCD();              // Updates the LCD display with remaining time
+    void on_resetButton_clicked(); // Slot for Cancel/Reset button
+    void on_okButton_clicked();    // Slot for OK button
+    void resetTimer();             // Function to reset the timer
 
 private:
     Ui::Timer *ui;
+    QTimer *timer;        // Timer for the countdown
+    int remainingTime;    // Remaining time in seconds
 };
 
 #endif // TIMER_H
+
